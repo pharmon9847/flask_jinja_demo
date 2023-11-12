0ca0e060ba2c679bcedddc9d4874c113a3955c2c
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-11-12 00:58:45 -0500

    added 'lucky' route and logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from random import randint
 
 app = Flask(__name__)
 
@@ -11,3 +12,9 @@ debug = DebugToolbarExtension(app)
 def say_hello():
     """Shows hello page"""
     return render_template('hello.html')
+
+
+@app.route('/lucky')
+def lucky_number():
+    num = randint(1, 100)
+    return render_template('lucky.html', lucky_num=num, msg='You are SO lucky!')
