a99552e4cb5443437bb226b1c17ad8b2c269f060
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-11-12 01:18:32 -0500

    added greeting route plus code

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from random import randint
+from random import randint, choice
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Wyatt_Pepper_Briar'
 debug = DebugToolbarExtension(app)
+
+COMPLIMENTS = ["cool", "clever", "tenacious", "awesome", "Pythonic"]
 
 
 @app.route('/hello')
@@ -18,3 +20,15 @@ def say_hello():
 def lucky_number():
     num = randint(1, 100)
     return render_template('lucky.html', lucky_num=num, msg='You are SO lucky!')
+
+
+@app.route('/form')
+def show_form():
+    return render_template('form.html')
+
+
+@app.route('/greet')
+def get_greeting():
+    username = request.args['username']
+    nice_thing = choice(COMPLIMENTS)
+    return render_template('greet.html', username=username, compliment=nice_thing)
