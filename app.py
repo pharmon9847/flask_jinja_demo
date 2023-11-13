b96ed40c63bf79acb40a313bdffaef77b6512b58
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-11-12 22:49:08 -0500

    Added a second form and greeter route

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from random import randint, choice
+from random import randint, choice, sample
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Wyatt_Pepper_Briar'
 debug = DebugToolbarExtension(app)
 
-COMPLIMENTS = ["cool", "clever", "tenacious", "awesome", "Pythonic"]
+COMPLIMENTS = ["bad ass!", "stellar!", "fucking awesome!",
+               "most righteous!", "killer!", "on point!"]
 
 
 @app.route('/hello')
@@ -32,3 +33,22 @@ def get_greeting():
     username = request.args['username']
     nice_thing = choice(COMPLIMENTS)
     return render_template('greet.html', username=username, compliment=nice_thing)
+
+
+@app.route('/spell/<word>')
+def spell_word(word):
+    caps_word = word.upper()
+    return render_template('spell_word.html', word=caps_word)
+
+
+@app.route('/form-2')
+def show_form_2():
+    return render_template('form-2.html')
+
+
+@app.route('/greet-2')
+def get_greeting_2():
+    username = request.args["username"]
+    wants = request.args.get('wants_compliments')
+    nice_things = sample(COMPLIMENTS, 3)
+    return render_template('greet_2.html', username=username, wants_compliments=wants, compliments=nice_things)
